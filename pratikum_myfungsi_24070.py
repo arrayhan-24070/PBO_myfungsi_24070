c9759a22a33610739a0c3ffab6ecc9b2b14808c1
arrayhan-24070 <arrayhanary78@gmail.com>
2025-12-16 23:45:26 +0900

    Add files via upload

---
+++ pratikum_myfungsi_24070.py
@@ -0,0 +1,6 @@
+
+def asal_sekolah(arrayhan, sekolah="SMA CITRA UMMAT"):
+ print(f"Helo, {arrayhan} ! kamu berasal dari {sekolah}.")
+
+asal_sekolah ("arrayhan")
+asal_sekolah ("arrayhan", "SMA CITRA UMMAT")
